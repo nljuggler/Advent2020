bfb2275c142f8d33f4422fe97e8fd81ea60f2d15
nljuggler <jeroen.de.kort@avivasolutions.nl>
2020-12-07 16:02:38 +0100

    Day 6

--- Advent2020/Days/Day06.cs
+++ Advent2020/Days/Day06.cs
@@ -1,24 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Advent2020.Helpers;
 
 namespace Advent2020.Days
 {
     class Day06 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsAssignment1Complete => false;
-        public bool IsAssignment2Complete => false;
+        public bool IsImplemented => true;
+        public bool IsAssignment1Complete => true;
+        public bool IsAssignment2Complete => true;
+        private List<string> Lines => DayFileReader.Read(6);
         public string GetAssignment1()
         {
-            throw new NotImplementedException();
+            return $"The sum of the first counts is: {GetGroups().Sum(grp => grp.Where(c => c != ';').Distinct().Count())}";
         }
 
         public string GetAssignment2()
         {
-            throw new NotImplementedException();
+            var result = GetGroups().Sum(grp => grp.Split(';').Select(s => s.ToCharArray()).Aggregate((x, y) => x.Intersect(y).ToArray()).Length);
+            return $"The sum of the second counts is: {result}";
+        }
+
+        private IEnumerable<string> GetGroups()
+        {
+            var group = string.Empty;
+            for (var index = 0; index < Lines.Count; index++)
+            {
+                var line = Lines[index];
+                group += $"{line};";
+                if (index == Lines.Count - 1 || string.IsNullOrEmpty(Lines[index + 1]))
+                {
+                    yield return group.Substring(0,group.Length - 1);
+                    group = string.Empty;
+                    index++;
+                }
+            }
         }
     }
 }
